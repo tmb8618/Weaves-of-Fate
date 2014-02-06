3053c4cf7b272662944965ece50561b158295cd2
TYLER <tmb8618@rit.edu>
2014-02-06 18:20:57 -0500

    getting this to work with openshift 8

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var path = require('path');
 
 var app = express();
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var ipaddr = process.env.OPENSHIFT_NODEJS_INTERNAL_IP || "127.0.0.1";
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 // all environments
 app.set('views', path.join(__dirname, 'views'));
